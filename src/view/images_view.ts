7c14d8a7a89885a93be6c2be6734a8812c673e10
henrique <henriquevaleirodsouza@hotmail.com>
2020-11-24 01:03:28 -0300

    Update images_view.ts

--- src/view/images_view.ts
+++ src/view/images_view.ts
@@ -4,7 +4,7 @@ export default {
 	render(image: Image) {
 		return {
 			id: image.id,
-			url: `https://oleopro.herokuapp.com//uploads/${image.path}`
+			url: `https://oleopro.herokuapp.com/uploads/${image.path}`
 		}
 	},
 
