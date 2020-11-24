25c3b2ad9ce80d9bfab29a91dc53d0fb6996a616
henrique <henriquevaleirodsouza@hotmail.com>
2020-11-23 23:56:16 -0300

    rota image

--- src/view/images_view.ts
+++ src/view/images_view.ts
@@ -4,7 +4,7 @@ export default {
 	render(image: Image) {
 		return {
 			id: image.id,
-			url: `http://localhost:3338/uploads/${image.path}`
+			url: `${process.env.APP_URL_IMAGE}/uploads/${image.path}`
 		}
 	},
 
